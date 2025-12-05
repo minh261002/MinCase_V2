9f50cef6338f81c363df28057e41d6563e37950c
Minh Tran <trcongminh.261002@gmail.com>
2025-12-05 20:01:58 +0700

    feat: add payment status retrieval and thank you page components

--- app/configure/preview/actions.ts
+++ app/configure/preview/actions.ts
@@ -80,6 +80,7 @@ export const createCheckoutSession = async ({
     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
     payment_method_types: ["card"],
     mode: "payment",
+    shipping_address_collection: { allowed_countries: ["VN"] },
     metadata: {
       userId: user.id,
       orderId: order.id,
