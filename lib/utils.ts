60efcfc583355ef5924c3ed6e6ed4236c601bdbb
Minh Tran <trcongminh.261002@gmail.com>
2025-12-05 21:46:40 +0700

    feat: refactor metadata handling and remove unused images

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -13,3 +14,34 @@ export const formatPrice = (price: number) => {
 
   return formatter.format(price);
 };
+
+export function constructMetadata({
+  title = "MinCase - custom high-quality phone cases",
+  description = "Create custom high-quality phone cases in seconds",
+  image = "/images/thumbnail.png",
+  icons = "/images/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@minh261002",
+    },
+    icons,
+    metadataBase: new URL("https://min-case.vercel.app/"),
+  };
+}
